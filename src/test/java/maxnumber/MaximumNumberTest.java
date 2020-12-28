007658a8d50d2950d44248a0b7bf51357216fbc7
Rajas1312 <you@example.com>
2020-12-28 23:36:59 +0530

    Added:code and test to find maximum float value

--- src/test/java/maxnumber/MaximumNumberTest.java
+++ src/test/java/maxnumber/MaximumNumberTest.java
@@ -24,4 +24,26 @@ public class MaximumNumberTest {
         int result=maximumNumber.max(1,2,3);
         Assert.assertEquals(3,result);
     }
+
+    @Test
+    public void givenMaxFloatValueAtFirstPlace_ShouldReturnMaxFloatValue () {
+        MaximumNumber maximumNumber=new MaximumNumber();
+        float result=maximumNumber.max(4.0f,1.0f,2.0f);
+        Assert.assertEquals(4.0f,result,0.0f);
+    }
+
+    @Test
+    public void givenFloatValueAtSecondPlace_ShouldReturnFloatValue() {
+        MaximumNumber maximumNumber=new MaximumNumber();
+        float result=maximumNumber.max(4.0f,5.0f,1.0f);
+        Assert.assertEquals(5.0f,result,0.0f);
+    }
+
+    @Test
+    public void givenFloatValueAtThirdPlace_ShouldReturnFloatValue(){
+        MaximumNumber maximumNumber=new MaximumNumber();
+        float result=maximumNumber.max(4.0f,5.0f,7.0f);
+        Assert.assertEquals(7.0f,result,0.0f);
+    }
+
 }
